d1dc495b45b24e35743f45f0c135e804f8971acd
James Koenig <Koenig.James@live.com>
2024-01-09 19:27:36 -0800

    move presentation attributes to css, refactor

--- src/app/svg/wireframe-lines.jsx
+++ src/app/svg/wireframe-lines.jsx
@@ -1,10 +1,12 @@
+import styles from './wireframe.module.css';
 import {lineParamsFromDiagonal} from '../../util/rectangle-combinatorics.js';
 
 const WireFrameLines = ({p1,p2}) =>
   lineParamsFromDiagonal(p1,p2).map( (params,idx) => (
-      <line {...params}
+      <line
+        className={styles.line}
         key={`line-${idx}`}
-        vectorEffect="non-scaling-stroke"
+        { ...params }
       />
     )
   );
--- src/app/svg/wireframe.jsx
+++ src/app/svg/wireframe.jsx
@@ -3,9 +3,7 @@ import WireFrameLines from './wireframe-lines.jsx';
 
 const WireFrame = () => (
   <svg className={styles.svg} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1' preserveAspectRatio='none'>
-    <g strokeWidth='2px' stroke='gray'>
-      <WireFrameLines p1={[0,0]} p2={[1,1]} />
-    </g>
+    <WireFrameLines p1={[0,0]} p2={[1,1]} />
   </svg>
 )
 
